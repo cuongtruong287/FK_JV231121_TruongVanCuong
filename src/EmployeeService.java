76cbe6450d8faba97b7398b22c347e58759321b8
Cuong Truong <cuongtruong287@gmail.com>
2024-03-14 23:53:29 +0900

    nop bai

--- src/EmployeeService.java
+++ src/EmployeeService.java
@@ -79,13 +79,13 @@ public class EmployeeService {
                         employee1.display();
                         System.out.println("Nhap thong tin moi");
                         System.out.println("Nhap ma nhan vien");
-                        employee1.setEmployeeID(Integer.parseInt(scanner.nextLine());
+                        employee1.setEmployeeID(Integer.parseInt(scanner.nextLine()));
                         System.out.println("Nhap ten nhan vien");
                         employee1.setEmployeeName(scanner.nextLine());
                         System.out.println("Nhap email cua nhan vien");
                         employee1.setEmail(scanner.nextLine());
                         System.out.println("Nhap sdt nhan vien");
-                        employee1.setPhoneNumber(Integer.parseInt(scanner.nextLine());
+                        employee1.setEmployeeID(Integer.parseInt(scanner.nextLine()));
                         System.out.println("Nhap dia chi nhan vien");
                         employee1.setAddress(scanner.nextLine());
                         System.out.println("Nhap gioi tinh nhan vien, nam: true, nu: false");
@@ -126,6 +126,6 @@ public class EmployeeService {
             if (employee.getEmployeeID() == ID){
                 return employee;
             }
-        }
+        } return  null;
     }
 }
